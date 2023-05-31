51f41cbcd3acca0e73e61c81b88bf96d969985c8
martinszkita <marpozniak1@gmail.com>
2023-05-31 14:12:50 +0200

    heapsort na intach dzala

--- PROJEKT 2/heapsort.cpp
+++ PROJEKT 2/heapsort.cpp
@@ -1,14 +1,9 @@
 #include <vector>
 #include <iostream>
-
-class MaxHeap {
+#include <iomanip>
+class MaxHeap
+{
 private:
-    std::vector<int> heap;
-
-    // Helper functions
-    void heapifyUp(int index);
-    void heapifyDown(int index);
-
 public:
     // Constructor
     MaxHeap();
@@ -18,25 +13,34 @@ public:
     int extractMax();
     void printHeap();
     void heapSort();
+
+    // Helper functions
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+    std::vector<int> heap;
+    bool isMax();
 };
 
 MaxHeap::MaxHeap() {}
 
-void MaxHeap::heapifyUp(int index) {
+void MaxHeap::heapifyUp(int index)
+{
     if (index <= 1)
         return;
 
-    int parent = index / 2;
+    int parent = (index - 1) / 2;
 
-    if (heap[index] > heap[parent]) {
+    if (heap[index] > heap[parent])
+    {
         std::swap(heap[index], heap[parent]);
         heapifyUp(parent);
     }
 }
 
-void MaxHeap::heapifyDown(int index) {
-    int leftChild = 2 * index;
-    int rightChild = 2 * index + 1;
+void MaxHeap::heapifyDown(int index)
+{
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
     int largest = index;
 
     if (leftChild < heap.size() && heap[leftChild] > heap[largest])
@@ -45,19 +49,32 @@ void MaxHeap::heapifyDown(int index) {
     if (rightChild < heap.size() && heap[rightChild] > heap[largest])
         largest = rightChild;
 
-    if (largest != index) {
+    if (largest != index)
+    {
         std::swap(heap[index], heap[largest]);
         heapifyDown(largest);
     }
 }
 
-void MaxHeap::insert(int value) {
+void MaxHeap::printHeap()
+{
+    for (int value : heap)
+    {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+}
+void MaxHeap::insert(int value)
+{
     heap.push_back(value);
     heapifyUp(heap.size() - 1);
+    // this->printHeap();
 }
 
-int MaxHeap::extractMax() {
-    if (heap.empty()) {
+int MaxHeap::extractMax()
+{
+    if (heap.empty())
+    {
         std::cout << "Heap is empty." << std::endl;
         return -1; // Or any appropriate handling of empty heap
     }
@@ -70,26 +87,44 @@ int MaxHeap::extractMax() {
     return maxElement;
 }
 
-void MaxHeap::printHeap() {
-    for (int value : heap) {
-        std::cout << value << " ";
+bool MaxHeap::isMax()
+{
+    for (int i = 1; i < heap.size(); i++)
+    {
+        int parent = (i - 1) / 2;
+        if (heap[i] > heap[parent])
+        {
+            return false;
+        }
     }
-    std::cout << std::endl;
-}
-
-void MaxHeap::heapSort(){
-    
+    return true;
 }
 
 
+void MaxHeap::heapSort()
+{
+    MaxHeap tmp;
+    while (!heap.empty())
+    {
+        tmp.heap.push_back(this->extractMax());
+    }
+    this->heap = tmp.heap;
+}
 
-int main() {
-    MaxHeap maxHeap;
-    maxHeap.insert(5);
-    maxHeap.insert(10);
-    maxHeap.insert(3);
-    maxHeap.insert(8);
+int main()
+{
+    system("clear");
+    srand(time(NULL));
+    MaxHeap h;
+    for (int i = 0; i < 10; i++)
+    {
+        h.insert(rand() % 100);
+    }
+    h.printHeap();
 
-    maxHeap.printHeap();
-
+    h.heapSort();
+    for (int i = h.heap.size() - 1; i >= 0; i--)
+    {
+        std::cout << h.heap[i] << " ";
+    }
 }
